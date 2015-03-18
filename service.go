391b6bb0a94ed64aab9e28ce4dd0e1d845b796b3
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-18 09:34:59 -0400

    added handler functions for token cruds

--- service.go
+++ service.go
@@ -26,10 +26,19 @@ func main() {
 	r.HandleFunc("/svc/clip/user/{email:[a-zA-Z0-9\\.\\-\\@]+}", clip.FetchUserHandler).Methods("GET")
 
 
-	r.HandleFunc("/svc/clip/team.json", clip.CreateTeamHandler).Methods("POST")
+	r.HandleFunc("/svc/clip/team", clip.CreateTeamHandler).Methods("POST")
 	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9\\.\\-_]+}", clip.UpdateTeamHandler).Methods("PUT")
 	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", clip.GetTeamHandler).Methods("GET")
 	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", clip.DeleteTeamHandler).Methods("DELETE")
+
+	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9\\.\\-_]+}/token",
+		clip.CreateTokenHandler).Methods("POST")
+	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
+		clip.UpdateTokenHandler).Methods("PUT")
+	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
+		clip.GetTokenHandler).Methods("GET")
+	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
+		clip.DeleteTokenHandler).Methods("DELETE")
 
 	
 	srv := &http.Server{
