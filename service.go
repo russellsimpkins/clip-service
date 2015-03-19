fee33449709386cd198ffd1099489936bb5aaaa8
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-19 15:41:18 -0400

    adjust how I do the routes

--- service.go
+++ service.go
@@ -16,30 +16,14 @@ func HelloWorld(writer http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	var (
+		router *mux.Router
+	)
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/svc/clip/user/{email:[a-zA-Z0-9\\.\\-\\@]+}", clip.CreateUserHandler).Methods("POST")
-	r.HandleFunc("/svc/clip/user/{email:[a-zA-Z0-9\\.\\-\\@]+}", clip.UpdateUserHandler).Methods("PUT")
-	r.HandleFunc("/svc/clip/user/{email:[a-zA-Z0-9\\.\\-\\@]+}", clip.FetchUserHandler).Methods("GET")
-
-	r.HandleFunc("/svc/clip/team", clip.CreateTeamHandler).Methods("POST")
-	r.HandleFunc("/svc/clip/teams", clip.GetTeamsHandler).Methods("GET")
-	r.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\.\\-_]+}", clip.UpdateTeamHandler).Methods("PUT")
-	r.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}", clip.GetTeamHandler).Methods("GET")
-	r.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}", clip.DeleteTeamHandler).Methods("DELETE")
-
-	r.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\.\\-_]+}/token",
-		clip.CreateTokenHandler).Methods("POST")
-	r.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
-		clip.UpdateTokenHandler).Methods("PUT")
-	r.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
-		clip.GetTokenHandler).Methods("GET")
-	r.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
-		clip.DeleteTokenHandler).Methods("DELETE")
-
+	router = mux.NewRouter()
+	clip.SetRoutes(router)
 	srv := &http.Server{
-		Handler:        r,
+		Handler:        router,
 		Addr:           ":8001",
 		WriteTimeout:   15 * time.Second,
 		ReadTimeout:    15 * time.Second,
