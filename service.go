e29c0b4274a6e22fdec1c15acac2a9f0a37c2115
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-19 12:44:50 +0000

    ran gofmt

--- service.go
+++ service.go
@@ -8,14 +8,12 @@ import (
 	"time"
 )
 
-
 func HelloWorld(writer http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	writer.Write([]byte("In it to win it"))
 	fmt.Printf("We have this many %v", vars)
 	return
 }
-
 
 func main() {
 
@@ -25,9 +23,8 @@ func main() {
 	r.HandleFunc("/svc/clip/user/{email:[a-zA-Z0-9\\.\\-\\@]+}", clip.UpdateUserHandler).Methods("PUT")
 	r.HandleFunc("/svc/clip/user/{email:[a-zA-Z0-9\\.\\-\\@]+}", clip.FetchUserHandler).Methods("GET")
 
-
 	r.HandleFunc("/svc/clip/team", clip.CreateTeamHandler).Methods("POST")
-	r.HandleFunc("/svc/clip/teams", GetTeamsHandler).Methods("GET")
+	r.HandleFunc("/svc/clip/teams", clip.GetTeamsHandler).Methods("GET")
 	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9\\.\\-_]+}", clip.UpdateTeamHandler).Methods("PUT")
 	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", clip.GetTeamHandler).Methods("GET")
 	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", clip.DeleteTeamHandler).Methods("DELETE")
@@ -41,17 +38,16 @@ func main() {
 	r.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
 		clip.DeleteTokenHandler).Methods("DELETE")
 
-	
 	srv := &http.Server{
-        Handler:        r,
-        Addr:           ":8001",
-        WriteTimeout:   15 * time.Second,
-        ReadTimeout:    15 * time.Second,
-        MaxHeaderBytes: 32768,
-    }
+		Handler:        r,
+		Addr:           ":8001",
+		WriteTimeout:   15 * time.Second,
+		ReadTimeout:    15 * time.Second,
+		MaxHeaderBytes: 32768,
+	}
 
-    err := srv.ListenAndServe()
-    if err != nil {
-        fmt.Println("ListenAndServe: ", err)
-    }
+	err := srv.ListenAndServe()
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
 }
